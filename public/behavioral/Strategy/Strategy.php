f3d1139d0f2f65f81cf4a28e6be18bf5e6a8b9e0
DarKsandr <gdarksandr@gmail.com>
2023-01-29 11:21:08 +0300

    Стратегия (Strategy)

---
+++ public/behavioral/Strategy/Strategy.php
@@ -0,0 +1,98 @@
+<?php
+
+namespace App\Behavioral\Strategy;
+
+/**
+ * Контекст определяет интерфейс, представляющий интерес для клиентов.
+ */
+class Context
+{
+    /**
+     * Контекст хранит ссылку на один из объектов Стратегии.
+     * Контекст не знает конкретного класса стратегии. Он должен работать со
+     * всеми стратегиями через интерфейс Стратегии.
+     */
+    private Strategy $strategy;
+
+    /**
+     * Обычно Контекст принимает стратегию через конструктор, а также
+     * предоставляет сеттер для её изменения во время выполнения.
+     */
+    public function __construct(Strategy $strategy)
+    {
+        $this->strategy = $strategy;
+    }
+
+    /**
+     * Обычно Контекст позволяет заменить объект Стратегии во время выполнения.
+     */
+    public function setStrategy(Strategy $strategy)
+    {
+        $this->strategy = $strategy;
+    }
+
+    /**
+     * Вместо того, чтобы самостоятельно реализовывать множественные версии
+     * алгоритма, Контекст делегирует некоторую работу объекту Стратегии.
+     */
+    public function doSomeBusinessLogic(): void
+    {
+        // ...
+
+        echo "Context: Сортировка данных с использованием стратегии (не знаю, как она это сделает)<br>";
+        $result = $this->strategy->doAlgorithm(["a", "b", "c", "d", "e"]);
+        echo implode(",", $result) . "<br>";
+
+        // ...
+    }
+}
+
+/**
+ * Интерфейс Стратегии объявляет операции, общие для всех поддерживаемых версий
+ * некоторого алгоритма.
+ *
+ * Контекст использует этот интерфейс для вызова алгоритма, определённого
+ * Конкретными Стратегиями.
+ */
+interface Strategy
+{
+    public function doAlgorithm(array $data): array;
+}
+
+/**
+ * Конкретные Стратегии реализуют алгоритм, следуя базовому интерфейсу
+ * Стратегии. Этот интерфейс делает их взаимозаменяемыми в Контексте.
+ */
+class ConcreteStrategyA implements Strategy
+{
+    public function doAlgorithm(array $data): array
+    {
+        sort($data);
+
+        return $data;
+    }
+}
+
+class ConcreteStrategyB implements Strategy
+{
+    public function doAlgorithm(array $data): array
+    {
+        rsort($data);
+
+        return $data;
+    }
+}
+
+/**
+ * Клиентский код выбирает конкретную стратегию и передаёт её в контекст. Клиент
+ * должен знать о различиях между стратегиями, чтобы сделать правильный выбор.
+ */
+$context = new Context(new ConcreteStrategyA());
+echo "Клиент: Стратегия настроена на нормальную сортировку.<br>";
+$context->doSomeBusinessLogic();
+
+echo "<br>";
+
+echo "Клиент: Стратегия настроена на обратную сортировку.<br>";
+$context->setStrategy(new ConcreteStrategyB());
+$context->doSomeBusinessLogic();
